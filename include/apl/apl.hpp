0bfbf5d4ba4b51c13227b6033a9d0cd22e8a8564
David Spry <dspry@gmx.com>
2023-10-05 19:37:29 +1100

    Add ASCII art

--- include/apl/apl.hpp
+++ include/apl/apl.hpp
@@ -1,6 +1,14 @@
-//! apl
-//! David Spry
-//! Created on 19/8/2023
+/**
+ *                       oooo
+ *                       `888
+ *   .oooo.   oo.ooooo.   888
+ *  `P  )88b   888' `88b  888
+ *   .oP"888   888   888  888
+ *  d8(  888   888   888  888
+ *  `Y888""8o  888bod8P' o888o
+ *             888
+ *            o888o
+ */
 
 #pragma once
 
